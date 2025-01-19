0a400966f95c491a6d38f9b5812e93e228e9c10c
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2025-01-19 19:36:04 -0300

    feat: used space widget

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,20 @@
-import { SearchIcon } from 'lucide-react'
+import { CogIcon, LifeBuoyIcon, SearchIcon } from 'lucide-react'
+import {
+  BarChartIcon,
+  CheckSquareIcon,
+  FlagIcon,
+  HomeIcon,
+  SquareStackIcon,
+  UsersIcon,
+} from 'lucide-react'
 
 import { SvgComponent } from './logo'
-import { MainNavigation } from './MainNavigation'
+import { NavItem } from './nav-item'
+import { UsedSpaceWidget } from './used-space-widget'
 
 export function Sidebar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <SvgComponent />
 
       <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
@@ -18,7 +27,23 @@ export function Sidebar() {
         />
       </div>
 
-      <MainNavigation />
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={HomeIcon} />
+        <NavItem title="Dashboard" icon={BarChartIcon} />
+        <NavItem title="Projects" icon={SquareStackIcon} />
+        <NavItem title="Tasks" icon={CheckSquareIcon} />
+        <NavItem title="Reporting" icon={FlagIcon} />
+        <NavItem title="Users" icon={UsersIcon} />
+      </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoyIcon} />
+          <NavItem title="Settings" icon={CogIcon} />
+        </nav>
+
+        <UsedSpaceWidget />
+      </div>
     </aside>
   )
 }
