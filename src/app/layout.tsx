59f18618a1d3898fd74ca6cb86bef85f84af662f
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2025-01-19 22:25:24 -0300

    feat: tabs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-import { Sidebar } from '@/components/Sidebar'
+import { Sidebar } from '@/components/sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="antialiased">
       <body className={inter.className} suppressHydrationWarning>
         <div className="grid-cols-app grid min-h-screen">
           <Sidebar />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { SettingsTabs } from '@/components/settings-tabs'
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="font-mono text-3xl underline">Home</h1>
-    </div>
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+      <SettingsTabs />
+    </>
   )
 }
